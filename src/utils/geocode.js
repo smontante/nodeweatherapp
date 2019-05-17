ad1a03f78c520b32c57fb04fbd78474d9bc4f802
smontante <shaunmontante@gmail.com>
2019-05-16 22:29:54 -0700

    first commit

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic21vbnRhbnRlIiwiYSI6ImNqdjZhOXZ3MzAzNzAzeW51Y3J1dmZvcWMifQ.3Xy3hlJja4PQT8so_7-srg&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
